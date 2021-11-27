3fff785d174a6bfc03c07e3a3805d8b84b2d1838
EC2 Default User <ec2-user@ip-172-31-81-143.ec2.internal>
2021-11-27 13:10:10 +0000

    Refactored again directory.rd

--- directory.rb
+++ directory.rb
@@ -1,18 +1,5 @@
-student_count = 11
 # first we print the list of students
-students = ["Dr. Hannibal Lecter",
-"Darth Vader",
-"Nurse Ratched",
-"Michael Corleone",
-"Alex DeLarge",
-"The Wicked Witch of the West",
-"Terminator",
-"Freddy Krueger",
-"The Joker",
-"Joffrey Baratheon",
-"Norman Bates",
-"The students of Villains Academy",
-"-----------------------------",
+students = [
 "Dr. Hannibal Lecter",
 "Darth Vader",
 "Nurse Ratched",
@@ -25,6 +12,10 @@ students = ["Dr. Hannibal Lecter",
 "Joffrey Baratheon",
 "Norman Bates"
 ]
+
+puts "The students of Villains Academy"
+puts "-----------------------------"
+
 students.each do |student|
    puts student 
 end
