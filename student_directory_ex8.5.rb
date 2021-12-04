e127e32d4c81a50e3e269e31e1c79359014ce539
EC2 Default User <ec2-user@ip-172-31-81-143.ec2.internal>
2021-12-04 09:42:20 +0000

    Adding  more information about student such as: hobbies, country of birth, height

---
+++ student_directory_ex8.5.rb
@@ -0,0 +1,56 @@
+#EX 5:
+#Our code only works with the student name and cohort.
+#Add more information: hobbies, country of birth, height, etc.
+def input_students
+  puts "Please enter the student's name, country of birth, height and hobbies: "
+  puts "To finish, just hit return five times."
+  #create an empty array
+  students = []
+  print "Name: "
+  name = gets.chomp
+  print "Country of Birth: "
+  country_of_birth = gets.chomp
+  print "Height (cm): "
+  height = gets.chomp
+  print "Hobbies: "
+  hobbies = gets.chomp
+  #while the name is not empty, repeat this code
+  while !name.empty? do
+    #add the student hash to the array
+    students << {name: name, cohort: :november, country_of_birth: country_of_birth, height: height, hobbies: hobbies}
+    puts "Now we have #{students.count} students"
+    #get another name from the user
+    print "Name: "
+    name = gets.chomp
+    print "Country of Birth: "
+    country_of_birth = gets.chomp
+    print "Height (cm): "
+    height = gets.chomp
+    print "Hobbies: "
+    hobbies = gets.chomp
+  end
+  #return the array of students
+  students
+end
+
+def print_header
+puts "The students of Villains Academy"
+puts "-------------"
+end
+
+def prints(students)
+students.each_with_index do |student, index|
+    new_index = index + 1
+  puts "Name: #{new_index}. #{student[:name]}, (#{student[:cohort]} cohort), Country of Birth: #{student[:country_of_birth]}, Height: #{student[:height]}, Hobbies: #{student[:hobbies]}"
+end
+end
+
+
+def print_footer(students)
+  puts "Overall, we have #{names.count} great students"
+end
+#nothing happens until we call the methods
+students = input_students
+print_header
+prints(students)
+print_footer(students)
